14173fe51be870427644dc6ba30138a51e7e9644
Steve <steve75918@gmail.com>
2024-05-07 05:12:21 +0800

    [Test] Add feature test for CalcTT2RaidTarget. - Knowledge of how multiple `$nextTick()` effect test. - Knowledge of `flushPromises()` is a better implement.

--- tests/src/components/CalcTT2RaidTarget.test.js
+++ tests/src/components/CalcTT2RaidTarget.test.js
@@ -1,7 +1,21 @@
 import { expect, describe, test, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
 import TT2RaidTarget from '../../../src/components/CalcTT2RaidTarget.vue'
 
+// mock API result
+const returnData = {
+  0: {name: 'Head'},
+  1: {name: 'Arm-1'},
+  3: {name: 'Torso'},
+  7: {name: 'Leg-1'},
+  4: {name: 'Arm-2'}
+}
+global.fetch = vi.fn().mockResolvedValue({
+  ok: true,
+  json: () => {
+    return Promise.resolve(returnData)
+  }
+})
 
 describe('Render', () => {
   test('renders form with inputs', () => {
@@ -47,24 +61,35 @@ describe('Unit', () => {
     }
   })
   test('handle submit', async () => {
-    // mock API result
-    const returnData = {
-      0: {name: 'Head'},
-      1: {name: 'Arm-1'},
-      3: {name: 'Torso'},
-      7: {name: 'Leg-1'},
-      4: {name: 'Arm-2'}
-    }
-    global.fetch = vi.fn().mockResolvedValueOnce({
-      ok: true,
-      json: () => {
-        return Promise.resolve(returnData)
-      }
-    })
     const component = mount(TT2RaidTarget)
 
     await component.vm.handleSubmit()
 
     expect(component.vm.result).toStrictEqual(returnData)
+  })
+})
+describe('Feature', () => {
+  test('input and check svg changed', async () => {
+    const component = mount(TT2RaidTarget)
+
+    // input test data
+    let inputs = component.findAll('input')
+    const testData = [1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99]
+
+    await Promise.all(inputs.map(async (input, index) => {
+      await input.setValue(testData[index])
+    }))
+
+    // submit
+    const form = component.find('form')
+
+    await form.trigger('submit')
+    await flushPromises()
+
+    // assert svg color changed
+    for (let part of Object.values(returnData)) {
+      let element = component.find({ref: part.name})
+      expect(element.attributes('fill')).toBe('green')
+    }
   })
 })
